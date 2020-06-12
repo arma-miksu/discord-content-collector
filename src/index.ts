fc6456b002d1a2e41f16d8d8dafa07c3bbfa1a86
Miksu <36083767+arma-miksu@users.noreply.github.com>
2020-06-12 19:57:09 +0300

    Clean up testing code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Client, MessageReaction, PartialUser, User, Message } from 'discord.js'
+import { Client } from 'discord.js'
 import nconf from 'nconf'
 
 nconf.argv().env().file({ file: 'config.json' })
@@ -8,49 +8,14 @@ if (!token) {
   throw Error('Missing required configuration: token')
 }
 
-console.log('Creating client')
-const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'USER', 'REACTION'] });
+console.log('Setting up client...')
+const client = new Client();
 
 client.on('ready', () => {
-  console.log('Client ready')
-})
-
-client.on('message', (message: Message) => {
-  console.log(
-    'Message',
-    message.guild?.toString(),
-    message.channel.toString(),
-    message.author.toString(),
-    message.content.toString()
-  )
-})
-
-client.on('messageReactionAdd', async (reaction: MessageReaction, user) => {
-  if (reaction.partial) {
-    try {
-      await reaction.fetch()
-    } catch (err) {
-      console.log('Failed fetching partial reaction', err)
-      return;
-    }
-  }
-  if (user.partial) {
-    try {
-      await user.fetch()
-    } catch (err) {
-      console.log('Failed fetching partial user', err)
-      return;
-    }
-  }
-  console.log(
-    'Reaction',
-    reaction.message.guild?.toString(),
-    reaction.message.channel.toString(),
-    user.toString(), reaction.emoji.toString()
-  )
+  console.log('Client ready...')
 })
 
 client.login(token)
   .catch(reason => {
-    console.log('Failed logging in', reason)
+    console.log('Failed to login...', reason)
   })
